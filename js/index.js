ad17b4226e226369c1337d6217788493a89f5e1f
Akere Adetola <adetolaakere1@gmail.com>
2019-09-03 15:10:58 +0100

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+const logoText = document.querySelector(".logo-heading");
+const heading = document.getElementById("heading");
+
+logoText.addEventListener("mouseover", event => {
+  logoText.style.fontSize = "50px";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.fontSize = "";
+  }, 500);
+});
+
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(el =>
+  el.addEventListener("click", () => {
+    alert("you clicked a button");
+  })
+);
